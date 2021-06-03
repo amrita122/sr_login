24ae7ed9991259b1de5317e142b5ca4ac143331f
amrita <amritakumari87251@gmail.com>
2021-06-03 17:10:07 +0530

    this is my file

--- timepass/test.py
+++
@@ -1,7 +0,0 @@
-def fun(num1,num2):
-    return num1%num2
-
-x=int(input("enter frst no"))
-y=int(input("enter second no"))
-print(fun(x,y))
-
--- timepass/test1.py
+++
@@ -1,2 +0,0 @@
-x,y=input("enter no and age").split()
-print(f"your name is {x} and age is {y} ")
